b68258082d9eb53cc4d8cba86f6a41f2278a9592
Meriam Aziz <meriamaziz000@gmail.com>
2026-07-17 03:01:09 +0300

    Add GitHub Pages workflow

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,14 @@
-import { defineConfig } from 'vite'
-import react, { reactCompilerPreset } from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import babel from '@rolldown/plugin-babel'
+import { defineConfig } from "vite";
+import react, { reactCompilerPreset } from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import babel from "@rolldown/plugin-babel";
 
 export default defineConfig({
+  base: "/Meriam-Portfolio/",
+
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
     tailwindcss(),
   ],
-})
+});
